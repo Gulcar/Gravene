06537926389a26b16ae9994021e3d8ddc4446606
Gulcar <weplaymods@gmail.com>
2023-01-10 20:18:23 +0100

    glfw opened a window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,25 @@
 #include <fmt/core.h>
+#include <fmt/color.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	fmt::print("pozdravljen svet");
+	fmt::print("pozdravljen svet\n");
+
+	if (!glfwInit())
+		fmt::print(fg(fmt::color::red), "Failed to init glfw!\n");
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Gravene", nullptr, nullptr);
+	if (window == nullptr)
+		fmt::print(fg(fmt::color::red), "Failed to create a glfw window!\n");
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 }
