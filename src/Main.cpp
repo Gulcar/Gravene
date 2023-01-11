c9bf1dcbd4aed2b1a8b71e9caa4e2cd28d3572cf
Gulcar <weplaymods@gmail.com>
2023-01-11 18:35:05 +0100

    added glad

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,12 @@
 #include <fmt/core.h>
 #include <fmt/color.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
 
 int main()
 {
@@ -9,15 +15,30 @@ int main()
 	if (!glfwInit())
 		fmt::print(fg(fmt::color::red), "Failed to init glfw!\n");
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	GLFWwindow* window = glfwCreateWindow(1280, 720, "Gravene", nullptr, nullptr);
 	if (window == nullptr)
 		fmt::print(fg(fmt::color::red), "Failed to create a glfw window!\n");
 
 	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+	glViewport(0, 0, 1280, 720);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+			glfwSetWindowShouldClose(window, true);
+
+		glClearColor(1.0f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 	}
 
